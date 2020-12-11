5038a430b87a9e48f210924309773fa4fa079c30
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-12-11 12:35:31 -0500

    added api to routes

--- api/server.js
+++ api/server.js
@@ -9,9 +9,9 @@ const server = express();
 
 server.use(express.json())
 
-server.use("/projects", projectRouter)
-server.use("/resources", resourceRouter)
-server.use("/tasks", taskRouter)
+server.use("/api/projects", projectRouter)
+server.use("/api/resources", resourceRouter)
+server.use("/api/tasks", taskRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Get some projects done!</h2>`);
